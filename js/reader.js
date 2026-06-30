95ef2eebed6285053c375c2d8fa19335bdfe5a3a
NijikawaSatoki <nijikawasatoki00@outlook.com>
2026-06-30 12:15:26 -0400

    Started work on the chapter template page. More work needed, though.

--- js/reader.js
+++ js/reader.js
@@ -17,9 +17,12 @@ fontSelect.addEventListener("change", (event) => {
     if (event.target.value === "ArialDefault") {
         storyBody.style.fontFamily = "'Arial', sans-serif";
         console.log("Story font has been switched to Arial.\nHappy reading!");
-    } else {
-        storyBody.style.fontFamily = `${event.target.value}`;
-        console.log(`Story font has been switched to ${event.target.value}.\nHappy reading!`);
+    } else if (event.target.value === "Times New Roman") {
+        storyBody.style.fontFamily = "'Times New Roman', serif";
+        console.log("Story font has been switched to Times New Roman.\nHappy reading!");
+    } else if (event.target.value === "Courier New") {
+        storyBody.style.fontFamily = "'Courier New', monospaced";
+        console.log("Story font has been switched to Courier New.\nHappy reading!");
     }
 });
 
