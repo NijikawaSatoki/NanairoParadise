c2e4f3f091d2de323f39630b9ecb2527c18a8fdd
Satoki Nijikawa <nijikawasatoki00@outlook.com>
2026-06-30 13:52:56 -0400

    Chapter template is ready to use.

--- js/reader.js
+++ js/reader.js
@@ -2,7 +2,7 @@
  * Story Reader Scripts
  * =============================================================================
  *
- * Author: Wamo, Q'aemil (Niji System)
+ * Author: Wamo, Vihreääketty, Q'aemil (Niji System)
  *
  */
 
@@ -14,15 +14,28 @@ const storyBody = document.getElementById("StoryBody"); // So that <article> ele
 
 const fontSelect = document.querySelector(".font-select");
 fontSelect.addEventListener("change", (event) => {
-    if (event.target.value === "ArialDefault") {
-        storyBody.style.fontFamily = "'Arial', sans-serif";
-        console.log("Story font has been switched to Arial.\nHappy reading!");
-    } else if (event.target.value === "Times New Roman") {
-        storyBody.style.fontFamily = "'Times New Roman', serif";
-        console.log("Story font has been switched to Times New Roman.\nHappy reading!");
-    } else if (event.target.value === "Courier New") {
-        storyBody.style.fontFamily = "'Courier New', monospaced";
-        console.log("Story font has been switched to Courier New.\nHappy reading!");
+    let selectedFont = event.target.value;
+    switch (selectedFont) {
+        case "default":
+            storyBody.style.fontFamily = "'Arial', sans-serif";
+            console.log("Story font has been switched to Arial.\nHappy reading!");
+            break;
+        case "serif":
+            storyBody.style.fontFamily = "'Times New Roman', serif";
+            console.log("Story font has been switched to Times New Roman.\nHappy reading!");
+            break;
+        case "monospaced":
+            storyBody.style.fontFamily = "'Courier New', monospaced";
+            console.log("Story font has been switched to Courier New.\nHappy reading!");
+            break;
+        case "cursive":
+            storyBody.style.fontFamily = "'Comic Sans', 'Comic Sans MS', 'Comic Neue', cursive";
+            console.log("Story font has been switched to Comic Sans.\nHappy reading!");
+            break;
+        default:
+            alert("Please select a font from the list.");
+            console.log("Please select a font from the list.");
+        // End of switch
     }
 });
 
@@ -30,11 +43,23 @@ fontSelect.addEventListener("change", (event) => {
 
 const fontSizes = document.querySelector(".font-size");
 fontSizes.addEventListener("change", (event) => {
-    if (event.target.value === "default") {
-        storyBody.style.fontSize = "16px";
-        console.log("The font size for the story is now 16!");
-    } else {
-        storyBody.style.fontSize = `${event.target.value}`;
-        console.log(`The font size for the story is now ${event.target.value}!`);
+    let selectedFontSize = event.target.value;
+    switch (selectedFontSize) {
+        case "default":
+            storyBody.style.fontSize = "16px";
+            console.log("The font size is now 16 px.");
+            break;
+        case "18px":
+            storyBody.style.fontSize = "18px";
+            console.log("The font size is now 18 px.");
+            break;
+        case "24px":
+            storyBody.style.fontSize = "24px";
+            console.log("The font size is now 24 px.");
+            break;
+        default:
+            alert("Please select a font size from the list.");
+            console.log("Please select a font size from the list.");
+        // End of switch
     }
 });
