b263c84d077b73699a6b53cd68a1545f4dbf5ada
Satoki Nijikawa <nijikawasatoki00@outlook.com>
2026-07-21 02:16:55 -0400

    Implemented page warnings

---
+++ js/readerwarn.js
@@ -0,0 +1,28 @@
+/* =============================================================================
+ * Story Reader Scripts
+ * =============================================================================
+ * 
+ * Author: Niji System
+ *             Satoki Nijikawa
+ *             Wamo
+ *             Kiyoi
+ *             Vihreääketty
+ *             Hsiao Lyuhsien
+ *             Antiainehunt Õudus
+ *             Rinith
+ *             Q'aemil
+ * 
+ */
+
+// Warns the user of the contents of the page
+
+function PageContentWarning() {
+    alert(`Warning!
+This page contains contents not meant for some users.
+Viewer discretion is advised.`);
+    return 0;
+}
+
+window.onload = PageContentWarning();
+
+/* >>>>>> END OF FILE <<<<<< */
